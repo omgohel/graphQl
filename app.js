611cd97f8bd3c613ef13b2921c96717ac6975102
omgohel <Your GitHub Email>
2023-08-01 08:23:31 +0530

    first commit

--- app.js
+++ app.js
@@ -43,9 +43,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/feed", feedRoutes);
-app.use("/auth", authRoutes);
-
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
